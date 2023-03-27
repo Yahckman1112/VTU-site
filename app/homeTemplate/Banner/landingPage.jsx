0f1120361189cc204cafbddcabe017655f940995
Yahckman <adeniranajibade2014@gmail.com>
2023-03-27 05:48:22 -0700

    banner finnished

--- app/homeTemplate/Banner/landingPage.jsx
+++ app/homeTemplate/Banner/landingPage.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import img from "../../../public/images/phone.png";
 import styles from "./banner.module.scss";
-import { Image } from "next/image";
+import Image from "next/image";
 import Link from "next/link";
-
+import myImage from "./../../images/phone.png";
 function HomeBanner(props) {
   return (
     <div className={styles.banner}>
-      <h1>Banner</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 col-sm-6">
+            <div className={styles.side1}>
+            <p className={styles.para1}>Welcome to DataWheel  </p>
+
+            <p className={styles.para2}>Best Place to get virtual <em className={styles.em}>TopUp!</em> </p>
+
+
+              <Link href='/' className={styles.btn_reg}> Register Now</Link>
+            </div>
+          </div>
+          <div className="col-6 col-sm-6">
+            <div className="img">
+              <Image src={myImage} width="30px" height="30px" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -8,8 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <HomeBanner /> */}
-      {/* <Features /> */}
+      <HomeBanner />
+      <Features />
+      
     </main>
   );
 }
