29b877c118d559b7453956b704f8bfa4ebc9148d
Yahckman <adeniranajibade2014@gmail.com>
2023-04-07 01:49:59 -0700

    register body

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
 import Link from "next/link";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 // import { Fa } from 'react-icons/fa'
 
 import img from "./../images/courses-04.jpg";
@@ -14,50 +14,70 @@ function page(props) {
       <div className={styles.wrapper}>
         <div className={styles.inner}>
           <div className={styles.image_holder}>
-            <Image className={styles.img} src={img} alt="pix" width={"0px"} height={400} />
+            <Image
+              className={styles.img}
+              src={img}
+              alt="pix"
+              width={"0px"}
+              height={400}
+            />
           </div>
 
           <Form className={styles.form}>
             <p className={styles.para1}> Resgister </p>
 
+            <div className={styles.form_row}>
+              <Form.Group
+                className={` ${styles.form_row_1}  mb-3`}
+                controlId="formBasicEmail"
+              >
+                <Form.Control
+                  className={styles.form_control}
+                  type="text"
+                  placeholder="firstname"
+                />
+              </Form.Group>
+              <Form.Group
+                className={` ${styles.form_row_2}  mb-3`}
+                controlId="formBasicEmail"
+              >
+                <Form.Control
+                  className={styles.form_control}
+                  type="text"
+                  placeholder="LastName"
+                />
+              </Form.Group>
+            </div>
 
-    <div className={styles.form_row}>
+            <Form.Group className={` mb-3`} controlId="formBasicEmail">
+              <Form.Control
+                className={styles.form_control}
+                type="email"
+                placeholder="Email"
+              />
+            </Form.Group>
+            <Form.Group className={`   mb-3`} controlId="formBasicEmail">
+              <Form.Control
+                className={styles.form_control}
+                type="number"
+                placeholder="Phone Number"
+              />
+            </Form.Group>
 
-         <Form.Group className={` ${styles.form_row_1}  mb-3`} controlId="formBasicEmail">
-        <Form.Control className={styles.form_control} type="text" placeholder="firstname" />
-      </Form.Group>
-         <Form.Group className={` ${styles.form_row_2}  mb-3`} controlId="formBasicEmail">
-        <Form.Control className={styles.form_control} type="text" placeholder="LastName" />
-      </Form.Group>
+            <Form.Group className={`  mb-3`} controlId="formBasicEmail">
+              <Form.Control
+                className={styles.form_control}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Group>
 
-    </div>
-   
-
-         <Form.Group className={` mb-3`} controlId="formBasicEmail">
-        <Form.Control className={styles.form_control} type="email" placeholder="Email" />
-      </Form.Group>
-         <Form.Group className={`   mb-3`} controlId="formBasicEmail">
-        <Form.Control className={styles.form_control} type="number" placeholder="Phone Number" />
-      </Form.Group>
-
-    
-
-
-         <Form.Group className={`  mb-3`} controlId="formBasicEmail">
-        <Form.Control className={styles.form_control} type="password" placeholder="Password" />
-      </Form.Group>
-         
-
-     
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <button className={styles.btn} type="submit">
-        Submit
-
-  
-      </button>
-
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <button className={styles.btn} type="submit">
+              Submit
+            </button>
           </Form>
         </div>
       </div>
